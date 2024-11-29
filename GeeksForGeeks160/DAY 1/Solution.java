ff9c75d7e00e28f5c1b89dcbc3c19f1e5fc3d708
Uday Akash E M <160722308+udaycodes004@users.noreply.github.com>
2024-11-29 19:53:10 +0530

    Add files via upload

---
+++ GeeksForGeeks160/DAY 1/Solution.java
@@ -0,0 +1,25 @@
+
+// User function Template for Java
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        if (arr.length < 2) {
+        return -1;
+    }
+
+    int largest = Integer.MIN_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
+
+    for (int num : arr) {
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num > secondLargest && num != largest) {
+            secondLargest = num;
+        }
+    }
+
+    return (secondLargest == Integer.MIN_VALUE) ? -1 : secondLargest;
+}
+        
+    }

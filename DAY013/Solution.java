c418464ee9b1224ac077153ed82845ade1c7a01e
Uday Akash E M <160722308+udaycodes004@users.noreply.github.com>
2024-12-03 22:04:49 +0530

    Add files via upload

---
+++ DAY013/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        Arrays.sort(arr);
+
+        int res = 1;
+        
+        for (int i = 0; i < arr.length; i++) {
+           
+            if (arr[i] == res) {
+                res++;
+            }
+           
+            else if (arr[i] > res) {
+                break;
+            }
+        }
+        return res;
+    }
+      
+    }
